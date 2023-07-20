f5abfef08ec91a3276a22fe1dbd2e0caa2e88c53
Dennis Dittmann <40322643+dittmanndennis@users.noreply.github.com>
2023-07-20 22:13:25 +0200

    Create constants.py

---
+++ tests/constants.py
@@ -0,0 +1,14 @@
+
+INFLUXDB = {
+    'TOKEN': 'YOUR_TOKEN',
+    'ORG': 'YOUR_ORG',
+    'URL': 'YOUR_URL',
+    'BUCKET': 'YOUR_BUCKET',
+    'TEST_BUCKET': 'YOUR_TEST_BUCKET'
+}
+
+MQTT_BROKER = {
+    'HOST': 'broker.hivemq.com',
+    'TCP_PORT': 1883,
+    'TOPIC': 'YOUR_TOPIC'
+}
