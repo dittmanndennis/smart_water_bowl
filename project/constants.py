358c9a100285dd8a5a07e16ba8810ee8e803bbf6
Dennis Dittmann <40322643+dittmanndennis@users.noreply.github.com>
2023-07-20 22:15:12 +0200

    Create constants.py

---
+++ project/constants.py
@@ -0,0 +1,18 @@
+
+INFLUXDB = {
+    'TOKEN': 'YOUR_TOKEN',
+    'ORG': 'YOUR_ORG',
+    'URL': 'YOUR_URL',
+    'BUCKET': 'YOUR_BUCKET',
+    'FORECAST_BUCKET': 'YOUR_FORECAST_BUCKET'
+}
+
+MQTT_BROKER = {
+    'HOST': 'broker.hivemq.com',
+    'TCP_PORT': 1883,
+    'TOPIC': 'YOUR_TOPIC'
+}
+
+TELEGRAM_BOT = {
+    'TOKEN': 'YOUR_BOT_TOKEN'
+}
